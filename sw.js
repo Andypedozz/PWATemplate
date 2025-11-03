285e156d72b8f3f74fd088666250feca951923ca
andrea <andypedini02@gmail.com>
2025-11-03 19:29:51 +0100

    .

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "pwa-demo-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+/* self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+} */
+
+self.addEventListener("install", e => {
+	console.log("Installed")
+});
+
+self.addEventListener("fetch", e => {
+	console.log("Fetched")
+});
